129ea653ff246a08ee521faebc4f66096d6056a9
Ric <wangrj@weipaitang.com>
2020-07-02 16:39:48 +0800

    feat: router增加路由分类 规范路由

--- app/router.js
+++ app/router.js
@@ -1,20 +1,53 @@
 'use strict';
 
 /**
+ * 路由表
+ * @param {String} method 'all' 'post' 'get' 三种 all为post和get都适用
+ * @param {String} route 自带/ 填写字符串或不填
+ * @param {String} controller 选填 不填的话自动去route的参数
+ */
+const ROUTE_DATA_SOURCE = {
+  user: [
+    { method: 'all', route: 'list' },
+    { method: 'get', route: 'user', controller: 'index' },
+    { method: 'post', route: 'add' },
+    { method: 'post', route: 'update' },
+    { method: 'post', route: 'delete' },
+  ],
+  home: [
+    { method: 'get' },
+    { method: 'get', route: 'text' },
+  ],
+};
+
+// 支持get和post请求方法
+function allMethod(route, fn, Router) {
+  Router.post(route, fn);
+  Router.get(route, fn);
+}
+
+// 创建路由表
+function createRouter(app) {
+  const { router, controller } = app;
+  for (const firstRoute in ROUTE_DATA_SOURCE) {
+    const Router = router.namespace(`/${firstRoute}`);
+    ROUTE_DATA_SOURCE[firstRoute].forEach(item => {
+      const useController = controller[firstRoute][item.controller || item.route || 'index'];
+      console.log(firstRoute, item);
+      item.method === 'all' ? allMethod(`/${item.route || ''}`, useController, Router) : Router[item.method](`/${item.route || ''}`, useController);
+    });
+  }
+}
+
+/**
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
-  const { router, controller } = app;
-  function all(route, fn) {
-    router.post(route, fn);
-    router.get(route, fn);
-  }
-  router.get('/', controller.home.index);
-  router.get('/user', controller.user.index);
-  router.get('/list', controller.user.list);
-  router.post('/add', controller.user.add);
-  router.post('/update', controller.user.update);
-  router.post('/delete', controller.user.delete);
-  all('/id', controller.user.userById);
+  // const { router, controller } = app;
+  createRouter(app);
+  // userRouter(app);
+  // router.get('/', controller.home.index);
+  // router.get('/user', controller.user.index);
+  // all('/id', controller.user.userById);
   // router.get('/test', controller.home.test);
 };
